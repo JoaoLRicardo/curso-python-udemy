389a03510ad9882675713ce414aa7916ba31c6cd
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-10-02 20:35:30 -0300

    Create funcoes_args_kwargs.py

---
+++ AULAS/funcoes_args_kwargs.py
@@ -0,0 +1,38 @@
+# def func(*args):
+#     print(args)
+
+
+# lista = [1, 2, 3, 4, 5]
+# n1, n2, *n = lista
+# print(n1, n2, n)
+# print('-' * 30)
+# print(*lista, sep='**')
+
+
+# def func(*args):
+#     print(args)
+#     print(args[0])
+#     print(args[-1])
+#     print(len(args))
+
+
+# func(1, 2, 3, 4, 5)
+
+
+# def func(*args):
+#     args = list(args)
+#     args[0] = 20
+#     print(args)
+
+
+# func(1, 2, 3, 4, 5)
+
+
+def func(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+lista = [1, 2, 3, 4, 5]
+lista2 = [10, 20, 30, 40, 50]
+func(*lista, *lista2)
